a7ea4d020f7e2988b3b8bf14ee383616ac144b23
VoxlRampnt <gregoryjbull@gmail.com>
2019-09-18 11:04:41 +0200

    Update Program.cs

--- SimpleGameEngine/SimpleGameEngine/Program.cs
+++ SimpleGameEngine/SimpleGameEngine/Program.cs
@@ -54,76 +54,44 @@ class Program
                 break;
             }
 
-            /*  ------------------------------------------
-                4.1
-                ------------------------------------------   
-                -   Store the x position of the snake in a variable called prevSnakeX.
-                -   Store the y position of the snake in a variable called prevSnakeY.
-                -   Call the Update method of snake.
-
-                This way we can store the previous position of the snake before
-                we update it
-            */
-
-            /*  ------------------------------------------
-                4.2
-                ------------------------------------------   
-                Change the if statement below so that it will only be true
-                if either the x or y position of the snake has changed since
-                the last update.
-
-                This will allow us to only make updates to the game
-                if the snake moved.
-            */
-            if (true)
+            
+            int prevSnakeX, prevSnakeY;
+            prevSnakeX = snake.X;
+            prevSnakeY = snake.Y;
+            /
+            if (snake.X != prevSnakeX || snake.Y != prevSnakeY)
             {
 
-                /*  ------------------------------------------
-                    4.3
-                    ------------------------------------------   
-                    If the position of the snake is equal to the position of 
-                    the pickup (It's been picked up),  do the following:
+                if (snake.X == pickup.X & snake.Y == pickup.Y)
+                {
+                    score += 100;
+                    canvas.WriteMessageTop(score + "");
+                    snake.Speed += SPEED_INCREASE;
+                    snake.AddSegment();
+                    pickup.SetPosition();
+                }
 
-                    -   Add 100 to the score.
-                    -   Use the relevant method of cnavas to update the score 
-                        displaying at the top of the screen.
-                    -   Increase the speed of the snake with the value of SPEED_INCREASE.
-                    -   Use the relevant method to add a segment to the snake.
-                    -   Use the relevant method of the pickup to change 
-                        its position.
-                */
 
-                /*  ------------------------------------------
-                    4.4
-                    ------------------------------------------   
-                    If the IsSnakeInBounds mehtod returns false, or 
-                    the IsHeadTouchingBody method of the snake returns true,
-                    the game is over. Do the following:
-
-                    -   "Game Over :(" should be written to the bottom of the
-                        screen using the relevant method of the canvas object.
-                    -   Call the WriteLine method of the Console class.
-                    -   Call the Beep method of the Console class.
-                    -   Break out of the game loop.
-                */
+                
 
                 //This method call will update the positions of all the snake segments.
                 snake.UpdateSegments();
+                if (IsSnakeInBounds() == false || snake.IsHeadTouchingBody() == true)
+                {
+                    canvas.WriteMessageBottom("Game Over: " + "/n" + "Score" + score);
+                    Console.WriteLine();
+                    Console.Beep();
+                    break;
+                }
 
-                /*  ------------------------------------------
-                    4.5
-                    ------------------------------------------ 
-                    We have updated the positions of all the segments of the snake,  
-                    but now we need to draw the segements to the canvas.
-
-                    REMEMBER: We need to clear the character drawn at the previous
-                    segment position before we draw the new one.
-
-                    BONUS: See if you can alternate the colours of the segments 
-                    between Magenta and Green.
-                */
+               
 
                 //draw pickup
+                Clear();
+                foreach (SnakeSegment segment in snake.Segments)
+                {
+                    canvas.Draw(segment.X, segment.Y, segment.Character);
+                }
                 canvas.Draw(pickup.X, pickup.Y, '', ConsoleColor.Red);
             }
         }
@@ -131,12 +99,15 @@ class Program
 
     static bool IsSnakeInBounds()
     {
-        /*  ------------------------------------------
-            4.6
-            ------------------------------------------
-            Returns true if the snake's X and Y positions are 
-            within the bounds of the canvas and false if it's not
-        */
-        return false;
+        
+        if (snake.X == 0 | snake.X == WIDTH | snake.Y == 0 | snake.Y == HEIGHT) 
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
     }
 }
